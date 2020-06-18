5726dfdb414e380309dc085e0cc80ead5a915d9a
dosoung <soungdo0919@gmail.com>
2020-06-18 19:35:36 +0900

    list

--- exam/src/main/java/com/board/controller/BoardController.java
+++ exam/src/main/java/com/board/controller/BoardController.java
@@ -11,6 +11,8 @@ import com.board.service.BoardService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
+
 @Controller
 public class BoardController {
 
@@ -46,6 +48,14 @@ public class BoardController {
         }
 
         return "redirect:/board/list.do";
+    }
+
+    @GetMapping(value = "/board/list.do")
+    public String openBoardList(Model model) {
+        List<BoardDTO> boardList = boardService.getBoardList();
+        model.addAttribute("boardList", boardList);
+
+        return "board/list";
     }
 
 }
