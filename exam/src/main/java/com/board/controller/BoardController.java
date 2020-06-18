a624ab32048b459ec204d0abf6b886f2ecf3fea0
dosoung <soungdo0919@gmail.com>
2020-06-18 16:24:06 +0900

    register

--- exam/src/main/java/com/board/controller/BoardController.java
+++ exam/src/main/java/com/board/controller/BoardController.java
@@ -1,11 +1,13 @@
 package com.board.controller;
 
+import com.board.domain.BoardDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.board.service.BoardService;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class BoardController {
@@ -14,17 +16,17 @@ public class BoardController {
     private BoardService boardService;
 
     @GetMapping(value = "/board/write.do")
-    public String openBoardWrite(Model model) {
-
-        String title = "제목";
-        String content = "내용";
-        String writer = "홍길동";
-
-        model.addAttribute("t", title);
-        model.addAttribute("c", content);
-        model.addAttribute("w", writer);
+    public String openBoardWrite(@RequestParam(value = "idx", required = false) Long idx, Model model) {
+        if (idx == null) {
+            model.addAttribute("board", new BoardDTO());
+        } else {
+            BoardDTO board = boardService.getBoardDetail(idx);
+            if (board == null) {
+                return "redirect:/board/list.do";
+            }
+            model.addAttribute("board", board);
+        }
 
         return "board/write";
     }
-
 }
