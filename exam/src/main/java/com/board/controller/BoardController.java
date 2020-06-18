5f1721982ba5b79a0f1cc11eb213e3adda0edd29
dosoung <soungdo0919@gmail.com>
2020-06-18 16:46:11 +0900

    DB register

--- exam/src/main/java/com/board/controller/BoardController.java
+++ exam/src/main/java/com/board/controller/BoardController.java
@@ -2,11 +2,13 @@ package com.board.controller;
 
 import com.board.domain.BoardDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.board.service.BoardService;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
@@ -29,4 +31,21 @@ public class BoardController {
 
         return "board/write";
     }
+    @PostMapping(value = "/board/register.do")
+    public String registerBoard(final BoardDTO params) {
+        try {
+            boolean isRegistered = boardService.registerBoard(params);
+            if (isRegistered == false) {
+                // TODO => 게시글 등록에 실패하였다는 메시지를 전달
+            }
+        } catch (DataAccessException e) {
+            // TODO => 데이터베이스 처리 과정에 문제가 발생하였다는 메시지를 전달
+
+        } catch (Exception e) {
+            // TODO => 시스템에 문제가 발생하였다는 메시지를 전달
+        }
+
+        return "redirect:/board/list.do";
+    }
+
 }
